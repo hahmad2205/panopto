c9225e2605a329ade293192b6d190c21201d7557
hahmad2205 <hassaan.ahmad@cogentlabs.co>
2025-05-16 15:34:51 +0500

    Font updated

--- utils.py
+++ utils.py
@@ -164,7 +164,7 @@ def markdown_to_pdf(markdown_content):
                 }}
             }}
             body {{
-                font-family: DejaVu Sans, sans-serif;
+                font-family: inter, sans-serif;
             }}
             h2 {{
                 font-size: 24px;
