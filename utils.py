4ac69c566f283d37206f7d8abd78add6c7ebc05d
hahmad2205 <hassaan.ahmad@cogentlabs.co>
2025-05-16 17:05:59 +0500

    Font updated

--- utils.py
+++ utils.py
@@ -168,7 +168,7 @@ def markdown_to_pdf(markdown_content):
             }}
             h2 {{
                 font-size: 24px;
-                font-weight: 600;
+                font-weight: 500;
                 line-height: 1.3;
                 color: #202124;
                 margin-top: 1.2em;
